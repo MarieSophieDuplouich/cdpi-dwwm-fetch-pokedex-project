d8d4edfc5e52dbaa8a5625c64ed76814afc986d2
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-10-06 15:59:37 +0200

    partie gauche finie

--- assets/script.js
+++ assets/script.js
@@ -8,10 +8,10 @@ function main() {
 
             pokemons_arr.forEach(pokemon_obj => {
                 console.log(pokemon_obj.name);
-
+                  console.log(pokemon_obj.pokedexId);
 
                 const pokemon_elem = template_pokemon.content.cloneNode(true);
-
+                pokemon_elem.querySelector(".pokemon-id").textContent = pokemon_obj.pokedexId;
                 pokemon_elem.querySelector("p").textContent = pokemon_obj.name;
                 pokemon_elem.querySelector("img").src = pokemon_obj.image;
                 pokemon_elem.querySelector("img").alt = pokemon_obj.name;
