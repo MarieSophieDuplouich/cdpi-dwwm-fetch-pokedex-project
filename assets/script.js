4ca4c4eb7286c895abd7f0d78ffe41f4f43b0b9a
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-10-06 15:33:34 +0200

    fetch commencé partie gauche affichée

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+function main() {
+    fetch("https://pokebuildapi.fr/api/v1/pokemon/limit/100")
+        .then(response_obj => response_obj.json())
+        .then(pokemons_arr => {
+
+            const container = document.querySelector(".pokemon-list");
+            const template_pokemon = document.querySelector(".template-pokemon");
+
+            pokemons_arr.forEach(pokemon_obj => {
+                console.log(pokemon_obj.name);
+
+
+                const pokemon_elem = template_pokemon.content.cloneNode(true);
+
+                pokemon_elem.querySelector("p").textContent = pokemon_obj.name;
+                pokemon_elem.querySelector("img").src = pokemon_obj.image;
+                pokemon_elem.querySelector("img").alt = pokemon_obj.name;
+                container.appendChild(pokemon_elem);
+
+            });
+        });
+
+
+}
+
+main();
+
+
